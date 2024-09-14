d58277e22afd50435434f518d386b847249d7e20
Aakash1910 <aakash.kadiyan@strategycoglobal.com>
2024-09-14 10:54:27 +0530

    adding working directory of a calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+
+console.log("hello")
+let x = document.querySelectorAll("#mybtn").value
+console.log(x);
+
+
+// function addition (){}
+// function multiplication (){}
+// function subtraction (){}
+// function division (){}
+// function sqaure (){}
+// function sqaureRoot (){}
+// function modOpt (){}
+// function clear(){}
